5aead3291a164d20fafe051aadbc2b97fde6200e
Beirens Amélie <ameliebeirens@Host-001.lan>
2025-02-04 17:25:53 +0100

    [6] create component for Navigation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import './App.css';
-import Home from './views/home';
-import CountersIndex from './views/countersIndex';
-import CalculatorIndex from './views/calculatorIndex';
-
-import {Routes, Route, Link} from 'react-router-dom';
-
+import Navigation from './components/navigation'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/counters">Compteurs</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculatrice</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/counters" element={<CountersIndex />} />
-        <Route path="/calculator" element={<CalculatorIndex />} />
-      </Routes>
+      <Navigation />
     </div>
   );
 }
