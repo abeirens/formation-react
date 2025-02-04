b0cd904e804e5855e0eb7088d59c6969f40364d8
Beirens Amélie <ameliebeirens@Host-001.lan>
2025-02-04 15:28:04 +0100

    [5] add calculator

--- src/App.js
+++ src/App.js
@@ -1,32 +1,147 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import * as Buttons from "./buttons"
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState(false);
+  const [hasDecimal, setHasDecimal] = useState(false);
 
-  function add() {
-    setCounter(counter + 1)
+  const handleAction = (action, buttonValue) => {
+    switch (action) {
+      case "reinit":
+        reinit();
+        break;
+      case "undo":
+        undo();
+        break;
+      case "percent":
+        percent();
+        break;
+      case "addOperator":
+        addOperator(buttonValue);
+        break;
+      case "calculate":
+        calculate(buttonValue);
+        break;
+      case "addComma":
+        addComma(buttonValue);
+        break;
+      case "equals":
+        equals();
+        break;
+      default:
+        console.alert("Action inconnue :", action);
+    }
+  };
+
+  const reinit = () => {
+    setResult(0);
+    setHasDecimal(false);
   }
 
-  function remove() {
-    setCounter(counter - 1)
+  const undo = () => {
+    setResult(result.toString().slice(0, -1));
   }
 
-  function reset() {
-    setCounter(0)
+  const percent = () => {
+    const extractPercent = result.split(/([+\-/*])/);
+    if (extractPercent.length > 2) {
+      const firstValue = extractPercent[0];
+      const secondValue = extractPercent[2];
+      const symbol = extractPercent[1];
+      const percentValue = (firstValue * secondValue) / 100;
+      const newResult = firstValue + symbol + percentValue;
+      setResult(newResult);
+    } else {
+      const percentValue = result / 100;
+      setResult(percentValue);
+    }
   }
 
+  const equals = () => {
+    if (result === null) {
+      setResult(0);
+    } else {
+      const equal = !operator ? eval(result) : eval(result.slice(0, -1));
+
+      if(equal === Infinity) {
+        setResult('Impossible de diviser par 0');
+      } else {
+        if (equal.toString().split('.').length > 1 && equal.toString().split('.')[1].length > 3) {
+          setResult(equal.toFixed(3).toString());
+        } else {
+          setResult(equal.toString());
+        }
+      }
+    }
+    setOperator(false);
+  }
+
+  const addComma = (value) => {
+    if (!hasDecimal) {
+      if (result === 0) {
+         setResult(0 + value);
+      } else {
+        const lastChar = result.split('').slice(-1)[0];
+        console.log(lastChar)
+        if (result !== null && ['+', '-', '*', '/'].includes(lastChar)) {
+          setResult(result + 0 + value);
+        } else {
+          setResult(result + value);
+        }
+      }
+
+      setHasDecimal(true);
+    }
+  }
+
+  const calculate = (value) => {
+    if(result === 0) {
+      setResult(value)
+    } else {
+      setResult(result + value);
+    }
+
+    setOperator(false);
+  }
+
+  const addOperator = (value) => {
+    if(!operator) {
+      setResult(result + value);
+      setOperator(true);
+      setHasDecimal(false);
+    }
+  }
 
   return (
     <div className="App">
-      <h1>Compteur</h1>
-
-      <div>{counter}</div>
-
-      <button onClick={add}>+</button>
-      <button onClick={remove}>-</button>
-      <button onClick={reset}>Réinitialiser</button>
+      <div className="mt-5">
+        <h1 className="text-center mb-4">Calculatrice</h1>
+        <Container className="w-25 text-center">
+          <Form.Control type="text" placeholder="0" value={result} readOnly />
+          <Row className="row-cols-4 justify-content-end g-1 mt-2">
+            {Buttons.buttons.flat().map((button, index) => {
+              return (
+                <Col key={`counters-array-${index}`}>
+                  <Button
+                    className="w-100"
+                    onClick={() => handleAction(button.action, button.value)}
+                    variant={button.class}
+                  >
+                    {button.value}
+                  </Button>
+                </Col>
+              )
+            })}
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/buttons.js
@@ -0,0 +1,31 @@
+export const buttons = [
+  [
+    {value: 'AC', class: 'warning', action: "reinit"},
+    {value: 'undo', class: 'warning', action: "undo"},
+    {value: '%', class: 'warning', action: "percent"},
+    {value: '/', class: 'warning', action: "addOperator"}
+  ],
+  [
+    {value: '7', class: 'light', action: "calculate"},
+    {value: '8', class: 'light', action: "calculate"},
+    {value: '9', class: 'light', action: "calculate"},
+    {value: '*', class: 'warning', action: "addOperator"}
+  ],
+  [
+    {value: '4', class: 'light', action: "calculate"},
+    {value: '5', class: 'light', action: "calculate"},
+    {value: '6', class: 'light', action: "calculate"},
+    {value: '-', class: 'warning', action: "addOperator"}
+  ],
+  [
+    {value: '1', class: 'light', action: "calculate"},
+    {value: '2', class: 'light', action: "calculate"},
+    {value: '3', class: 'light', action: "calculate"},
+    {value: '+', class: 'warning', action: "addOperator"}
+  ],
+  [
+    {value: '0', class: 'light', action: "calculate"},
+    {value: '.', class: 'light', action: "addComma"},
+    {value: '=', class: 'danger', action: "equals"}
+  ]
+];
